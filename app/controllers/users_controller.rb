bd196d738d4f6a83aba17f48f0c72d3737e326e6
riririusei99 <riry1313@gmail.com>
2019-05-03 04:00:01 +0000

    add user action edit&update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!, only: :show
+  before_action :authenticate_user!, except: :sign_in
 
   def show
     @user = User.find(params[:id])
@@ -11,5 +11,15 @@ class UsersController < ApplicationController
   end
   
   def update
+    user = User.find(params[:id])
+    if user.your_account?(current_user)
+      user.update(update_params)
+      redirect_to action: :show
+    end
+  end
+  
+  private
+  def update_params
+    params.require(:user).permit(:nickname, :email)
   end
 end
