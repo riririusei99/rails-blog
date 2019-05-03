c9d41dc6dc404a1bf76d4372a6a4576e025d6603
riririusei99 <riry1313@gmail.com>
2019-05-03 03:21:23 +0000

    change routing file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # association
   has_many :posts
+  
+  def your_account?(user)
+    self.id == user.id
+  end
 end
