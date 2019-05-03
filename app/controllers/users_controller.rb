59126ebf6a496ec973704ffb027bd6ceab9b4d47
riririusei99 <riry1313@gmail.com>
2019-05-03 07:45:49 +0000

    add user page & template

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!, except: :sign_in
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:posts).find(params[:id])
+    @posts = User.find(params[:id]).posts
   end
   
   def edit
