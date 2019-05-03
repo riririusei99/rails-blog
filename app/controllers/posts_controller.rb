911b8d0039d732a115db651c8157528502b7d696
riririusei99 <riry1313@gmail.com>
2019-05-03 04:04:58 +0000

    fix bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = Post.order('updated_at DESC').page(params[:page]).per(5)
