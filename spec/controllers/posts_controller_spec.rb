407a880f71c2fb8f192beafea72d956d1bbf393b
riririusei99 <riry1313@gmail.com>
2019-05-04 06:01:51 +0000

    add test edit action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -15,4 +15,25 @@ describe PostsController do
     end
   end
 
+  describe 'GET #edit' do
+
+    it "assigns the requested contact to @post" do
+      # ユーザ作成・ログイン
+      user = create(:user)
+      login_user user
+      # 投稿を登録
+      post = create(:post, user_id: user.id)
+      get :edit,  params: { id: post.id }
+      expect(assigns(:post)).to eq post
+    end
+
+    it "renders the :edit template" do
+      user = create(:user)
+      login_user user
+      post = create(:post, user_id: user.id)
+      get :edit,  params: { id: post.id }
+      expect(response).to render_template :edit
+    end
+  end
+
 end
