226d37c90cbfa51f69b4c22badbcc16d2f224f26
riririusei99 <riry1313@gmail.com>
2019-05-04 01:27:16 +0000

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # association
   has_many :posts
-  
+
+  # validation
+  validates :nickname, presence: true, length: { maximum: 6 }
+
   def your_account?(user)
     self.id == user.id
   end
