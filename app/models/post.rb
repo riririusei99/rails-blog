f7a3cf3da9bb0d070641f7ee0d264b91ca4cd323
riririusei99 <riry1313@gmail.com>
2019-05-04 02:52:35 +0000

    add test

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   
   # validation
-  validates :title, :post ,presence: true
+  validates :title, :post, :user_id, presence: true
 
   def your_post?(user)
     self.user_id == user.id
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 describe User do
   describe '#create' do
     
+    it "is valid with title and post and user_id" do
+      post = build(:post)
+      create(:user, id: post[:user_id])
+      expect(post).to be_valid
+    end
+
     it "is invalid without title" do
       post = build(:post, title: "")
       post.valid?
@@ -16,5 +22,12 @@ describe User do
       expect(post.errors[:post]).to include("can't be blank")
     end
     
+    it "is invalid without user_id" do
+      post = build(:post, user_id: "")
+      post.valid?
+      puts post.errors
+      expect(post.errors[:user_id]).to include("can't be blank")
+    end
+
   end
 end
